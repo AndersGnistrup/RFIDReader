8df6e99c6384e09ef70c708f375dcd397d226273
anders Gnistrup <a.gnistrup@gmail.com>
2020-05-04 21:24:16 +0200

    Initial start of project.
    System is using a JTAG debugger but this can be changed.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * RfidReader.cpp
+ *
+ * Created: 04-05-2020 21:16:33
+ * Author : agn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
