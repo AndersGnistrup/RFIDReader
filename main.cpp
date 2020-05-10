ac08ca1b3608a92a8ea66905a02f19d5143a1875
anders Gnistrup <a.gnistrup@gmail.com>
2020-05-10 17:36:51 +0200

    Added UART library from github.
    This seems to be the most upto date candidate. Read the license info in
    uart.h (It's GPLv2). Library could be updated in the future. The current
    version usied is:
    
    	commit 6773f425747eae3cecd49c28cf8c440f18b4ea99
    	Author: andygock <andy@agock.com>
    	Date:   Tue Apr 11 00:26:13 2017 +1000
    
    	    Update docs
    
    The library is interrupt driven which means that it is much more easy to
    do handle multiply input/output.

--- main.cpp
+++ main.cpp
@@ -3,16 +3,25 @@
  *
  * Created: 04-05-2020 21:16:33
  * Author : agn
- */ 
+ */
 
+extern "C" {
+	#include "uart/uart.h"
+}
 #include <avr/io.h>
+#include <util/delay.h>
 
+// Select baud rate on the uart
+#define UART_SPEED_115200 UART_BAUD_SELECT_DOUBLE_SPEED(115200, 16000000)
 
 int main(void)
 {
+	uart0_init(UART_SPEED_115200);
     /* Replace with your application code */
     while (1) 
     {
+		_delay_ms(1000);
+		uart_puts("Hello world\r\n");
     }
 }
 
