dacbb78affd8183fb37b74926c193d8ed2ba89a2
Anders Gnistrup <a.gnistrup@gmail.com>
2022-03-26 20:56:27 +0100

    Is not working

--- Core/Src/RfidReader.c
+++ Core/Src/RfidReader.c
@@ -11,12 +11,14 @@
 
 
 static int count = 0;
+static int readings = 0;
 /**
   * @brief This function handles EXTI line4 interrupt.
   */
 void EXTI4_IRQHandler(void)
 {
     count++;
+    ClearBitMask(CommIrqReg, 0x80);
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
 }
 
@@ -30,6 +32,8 @@ RfidReader * rfidReaderInit(SPI_HandleTypeDef *spiDev)
     RfidReader* reader = malloc(sizeof(RfidReader_));
     reader->dev = spiDev;
     MFRC522_Init(spiDev);
+    ClearBitMask(CommIrqReg, 0x80);
+    SetBitMask(CommIrqReg, 0x7F);
     return reader;
 }
 
@@ -46,8 +50,9 @@ void rfidReaderLoop(RfidReader *ctx)
     status = MFRC522_Request(PICC_REQIDL, cardstr);
     if (status == MI_OK)
     {
-        len = snprintf(&str[len], sizeof(str), "Card(%d):\r\n\tVersion: %x\r\n\tType: %x,%x,%x\r\n",
-                count, version, cardstr[0], cardstr[1], cardstr[2]);
+        readings++;
+        len = snprintf(&str[len], sizeof(str), "Card(%d/%d):\r\n\tVersion: %x\r\n\tType: %x,%x,%x\r\n",
+                count, readings, version, cardstr[0], cardstr[1], cardstr[2]);
         status = MFRC522_Anticoll(cardstr);
         if(status == MI_OK)
         {
